42b282d215cb141d4ffe0e5e049f8233575f2b67
Bruno Kenj <bruno.kenj@gmail.com>
2017-07-17 22:39:51 -0300

    changes do eth

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
 
-var darkcoin = require('./lib');
+var ethcoin = require('./lib');
 
-var client = new darkcoin.Client({
-  host: 'localhost',
-  port: 15715,
-  user: 'yourusername',
-  pass: 'youpassword'
+var client = new ethcoin.Client({
+  host: '127.0.0.1',
+  port: 8545,
+  user: '',
+  pass: ''
 });
 
-client.cmd('getbalance', '*', 6, function(err, balance){
+client.cmd('eth_accounts', '*', 6, function(err, balance){
   if (err) return console.log(err);
   console.log('Balance:', balance);
 });
